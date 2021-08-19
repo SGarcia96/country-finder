1e52020a2473913b03bd034df36d8821eb409d1d
sgarcia <sgcerverdadev@gmail.com>
2021-08-19 16:00:33 +0200

    const api key changed to REACT_APP_WEATHER_API_KEY

--- src/services/weather/getCurrentWeather.js
+++ src/services/weather/getCurrentWeather.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
-const api_key = process.env.REACT_APP_WEATHER_API_KEY
+const REACT_APP_WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY
 
 export const getCurrentWeather = async (capital) => {
-  const baseUrl = `http://api.weatherstack.com/current?access_key=${api_key}&query=${capital}`
+  const baseUrl = `http://api.weatherstack.com/current?access_key=${REACT_APP_WEATHER_API_KEY}&query=${capital}`
 
   return await axios.get(baseUrl).then((response) => {
     return response.data.current
